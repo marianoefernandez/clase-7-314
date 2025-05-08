d582744a1ff9b587ef1b33b340585e80eeb1ab8d
marianoefernandez <marianofernandez@MacBook-Air-de-Mariano.local>
2025-05-07 21:02:16 -0300

    Primer vesion

---
+++ Funciones.py
@@ -0,0 +1,71 @@
+import random
+
+def mostrar_empleados(array_legajos:list, array_sueldos:list,array_nombres:list,array_edades:list) -> None:
+    for i in range(len(array_legajos)):
+        print(f"MOSTRANDO EMPLEADO {i+1}")
+        mostrar_empleado(array_legajos,array_sueldos,array_nombres,array_edades,i)
+        print("")
+        
+def cargar_empleados(array_legajos:list, array_sueldos:list,array_nombres:list,array_edades:list) -> bool :
+    retorno = False
+    
+    for i in range(len(array_edades)):
+        print(f"CARGANDO EMPLEADO {i+1}")
+        nombre = input("Ingrese su nombre: ")
+        #Validar esto
+        edad = int(input("Ingrese su edad: "))
+        #Validar
+        sueldo = float(input("Ingrese su sueldo: "))
+        #Validar
+        #El legajo no se pide, el sistema lo genera
+        #Puede ser autoincremental 
+        #Random
+        legajo = random.randint(11111,99999)
+        
+        array_legajos[i] = legajo
+        array_nombres[i] = nombre
+        array_sueldos[i] = sueldo
+        array_edades[i] = edad
+        
+        print("EMPLEADO CARGADO")
+        
+        retorno = True
+        
+    return retorno
+        
+def sumar_array(array:list) -> float | int:
+    acumulador = 0
+    for i in range(len(array)):
+        if type(array[i]) == int or type(array[i]) == float:
+            acumulador += array[i]
+    
+    return acumulador
+
+def calcular_promedio(acumulador:float | int, contador:int) -> float | None:
+    if contador != 0:
+        promedio = acumulador / contador
+    else:
+        promedio = None
+        
+    return promedio
+
+def buscar_maximo_array(array:list) -> int:
+    bandera = False
+    
+    for i in range(len(array)):
+        if bandera == False:
+            maximo = array[i]
+            indice = i
+            bandera = True
+        else:
+            if array[i] > maximo:
+                maximo = array[i]
+                indice = i 
+                
+    return indice
+
+def mostrar_empleado(array_legajos:list, array_sueldos:list,array_nombres:list,array_edades:list,indice_empleado:int) -> None:
+    print(f"Legajo: {array_legajos[indice_empleado]}")
+    print(f"Nombre: {array_nombres[indice_empleado]}")
+    print(f"Edad: {array_edades[indice_empleado]}")
+    print(f"Sueldo: $ {array_sueldos[indice_empleado]}")
---
+++ Main.py
@@ -0,0 +1,72 @@
+#Necesitamos un sistema para alojar a los empleados de la empresa , los mismos tienen que tener un legajo, un nombre, su edad y el sueldo que tienen
+#Dentro de la empresa hay 3 empleados
+
+#Aplicamos el concepto de arrays paralelos para estructurar los datos
+#Son distintos arrays que estan relacionados entre si por su indice, cada uno de estos represanta un dato en particular
+#Estos mismos deben tener la misma cantidad de elementos
+
+from Funciones import *
+import os
+
+array_legajos = [0,0,0]
+array_nombres = [0,0,0]
+array_edades = [0,0,0]
+array_sueldos = [0,0,0]
+bandera_carga = False
+
+#MOSTRANDO
+
+# mostrar_empleados(array_legajos,array_sueldos,array_nombres,array_edades)
+# cargar_empleados(array_legajos,array_sueldos,array_nombres,array_edades)
+# mostrar_empleados(array_legajos,array_sueldos,array_nombres,array_edades)
+
+#1.Cargar empleados
+#2.Mostrar empleados
+#3.Calcular promedio de sueldos
+#4.Mostrar el empleado con mayor salario
+#5.Salir
+
+while True:
+    #Muestra el menu
+    print("1.Cargar Empleados \n2.Mostrar Empleados\n3.Calcular promedio sueldos\n4.Mostrar el empleado con mayor salario\n5.Salir")
+    #Me pide la opcion a elegir
+    opcion = int(input("Ingrese la opcion: "))
+    while opcion > 5 or opcion < 1:
+        print("OPCION ERRONEA")
+        opcion = int(input("Reingrese la opcion(1-5): "))
+    #La elige
+    if opcion == 1:
+        respuesta = cargar_empleados(array_legajos,array_sueldos,array_nombres,array_edades)
+        
+        if respuesta == True:
+            print("EMPLEADOS CARGADOS CON EXITO")
+            bandera_carga = True
+        else:
+            print("HUBO ERROR AL CARGAR")
+    elif opcion == 2:
+        if bandera_carga == True:
+            mostrar_empleados(array_legajos,array_sueldos,array_nombres,array_edades)
+        else:
+            print("No se pueden mostrar los empleados porque no fueron cargados")
+    elif opcion == 3:
+        if bandera_carga == True:
+            suma_sueldos = sumar_array(array_sueldos)
+            promedio = calcular_promedio(suma_sueldos,len(array_sueldos))
+            print(f"El promedio de sueldos es de ${promedio}")
+        else:
+            print("No es posible calcular el promedio de sueldos porque no se cargo ningun empleado")
+    elif opcion == 4:
+        if bandera_carga == True:
+            print("MOSTRANDO AL EMPLEADO CON MAYOR SUELDO")
+            indice_mayor_sueldo = buscar_maximo_array(array_sueldos)
+            mostrar_empleado(array_legajos,array_sueldos,array_nombres,array_edades,indice_mayor_sueldo)
+        else:
+            print("No se puede mostrar el empleado con mayor sueldo porque no se cargo nada.")
+    else:
+        print("Saliendo...")
+        break
+    
+    #Punto de control en el que usuario tiene que tocar cualquier tecla para continuar
+    input("Presione cualquier tecla para continuar...")
+    #os.system("cls")
+    os.system("clear")
